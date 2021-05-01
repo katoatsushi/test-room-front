c964a6a7fd5485060a611e7d5e9910b756ee12f4
kato atsushi <ka.baseball1997@gmail.com>
2021-05-01 21:38:51 +0900

    ss

--- src/components/appointments/appointment_new_func.js
+++ src/components/appointments/appointment_new_func.js
@@ -79,7 +79,7 @@ const AppointmentNew = (props) => {
         <React.Fragment key={index}>
                 <FormControlLabel 
                     value={time[0][0]}
-                    disabled={(time[1] == 0)}
+                    disabled={(time[1] <= 0)}
                     onClick={() => handleTimeSelect(time)}
                     control={<Radio />}
                     label= {time[1] + "枠" + time[2][0][0]+ "時"+ time[2][0][1] + "分" + " 〜 " + time[2][1][0] + "時" + time[2][1][1] + "分"}
